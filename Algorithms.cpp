3458c5526e8147f8e2062e5fc799bc0cc980171e
Zeke Kaufman <zkaufman@zeke.local>
2012-10-15 23:07:57 -0400

    - Added GSBS algorithm. - Better comments.

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -5,15 +5,38 @@ using namespace std;
 
 class Algorithms {
 public:
+  static int MI(int, int);
   static int GDC(int, int);
   static int FEA(int, int, int);
   static std::vector<int> Z(int);
   static std::vector<int> primitiveInts(int);
 };
 
+// Multipilicative Inverse (brute force).
+// Returns the mulitplicitive inverse of x
+// such that: ax = by + 1. Returns a.
+// Example:
+//   MI(200, 313) is: 36
+//   (36)(200) = (somenumber)(313) + 1
+int MI(int base, int mod) {
+  int remainder = 0;
+
+  for(int b = 0; b < 100 ; b++) {
+    for (int a = 0; a < 100; a++) {
+      remainder = (base * a) - (mod * b);
+      if (remainder == 1)
+        return a;
+    }
+  }
+
+  return -1;
+}
+
 // Greatest common divisor.
 // x = the bigger number
 // y = the smaller number
+// Example:
+//    GCD(899, 1914) is: 29
 int GCD(int x, int y) {
 
   while (y != 0) {
@@ -27,17 +50,14 @@ int GCD(int x, int y) {
 
 // Calculates the group Z sub n
 // Example:
-//   n = 14
-//   Z_n( n ) = { 1, 3, 5, 9, 11, 13 }
+//   Z_n(14) is: { 1, 3, 5, 9, 11, 13 }
 std::vector<int> Z_n(int n) {
-
   vector< int > Z;
 
   for ( int i=0 ; i < n ; i++ ) {
     int gcd = GCD(i,n);
-    if (gcd == 1) {
+    if (gcd == 1)
       Z.push_back(i);
-    }
   }
 
   return Z;
@@ -46,10 +66,9 @@ std::vector<int> Z_n(int n) {
 // Fast Exponentiation algorithm.
 // Returns x^e mod m
 // Example:
-//   x = 8, e = 2, m = 13
+//   FEA(8, 2, 13) is: 12
 //   (8)^2 % 13 = 12
 int FEA(int x, int e, int m) {
-
   int Y = 1;
 
   while (e != 0) {
@@ -67,10 +86,8 @@ int FEA(int x, int e, int m) {
 
 // Calculates the primitive roots of a number n.
 // Example:
-//   n = 26
-//   primitive rootes = { 7 11 15 19 }
+//   primitiveRoots(26) is: { 7 11 15 19 }
 std::vector<int> primitiveRoots(int n) {
-
   vector< int > primitive_roots;
   std::vector<int> Z = Z_n(n);
   int phi = Z.size();
@@ -87,7 +104,6 @@ std::vector<int> primitiveRoots(int n) {
       cout << val << "   ";
     }
     cout << endl;
-
     if (Z_temp.size() == 0)
       primitive_roots.push_back(z);
   }
@@ -95,21 +111,73 @@ std::vector<int> primitiveRoots(int n) {
   return primitive_roots;
 }
 
+// Baby-step Giant-step Algorithm.
+// Calulates log_b(x) mod p.
+// Example:
+//    BSGS(2,3,101) is: 69
+//    Log_2(3) mod 101 = 69
+int BSGS(int logbase, int x, int mod) {
+  int m = floor( sqrt(mod - 1) );
+  vector<int> vec(m);
+
+  // Calculate c.
+  int mult_inv = MI(logbase, mod);
+  int c = FEA(mult_inv, m, mod);
+  cout << "c = " << c << endl;
+
+  for ( int i = 0 ; i < m ; i++ ) {
+    vec[i] = FEA(logbase,i,mod);
+    cout << i << ", " << vec[i] << endl;
+  }
+
+  // vec should be sorted based on the second element
+  // of the pair. Skippimng that step for now.
+  // sort(vec)
+
+  int temp_x = x;
+  int j;
+  int i = 0;
+  vector<int>::iterator it;
+  while (i < m) {
+    it = std::find(vec.begin(), vec.end(), temp_x);
+
+    cout << "temp_x = " << temp_x << ", it = " << (*it) << ", other = " << std::distance(vec.begin(), it) << endl;
+    if (*it != 0) {
+      j = it - vec.begin();
+      break;
+    }
+    temp_x = (temp_x * c) % mod;
+    i++;
+  }
+
+  cout << "log_" << logbase << "(" << x << ")" << " % " << mod << " = " <<
+          m << " * " << i << " + " << j << " = ";
+  if (j==0)
+    return 0;
+  else
+    return (m*i + j);
+}
+
 int main(int argc, char *argv[]) {
   cout << "Running in C++" << endl;
 
-  // Test GCD code.
-  int x = 1024;
-  int y = 888;
+  // Test Multiplicative Inverse.
+  int base = 200;
+  int mod = 313;
+  cout << "Muyltiplicative Inverse for " << base << " mod " << mod << " = " << MI(base, mod) << endl;
+
+  // Test Greatest Common Denominator code.
+  int x = 899;
+  int y = 1914;
   int gcd = GCD(x, y);
   cout << "GCD for  " << x << " and " << y << " is: " << gcd << endl;
 
   // Test Fast Exponentiation Algorithm code.
-  int base = 8;
-  int e = 2;
-  int m = 13;
+  base = 11;
+  int e = 9;
+  int m = 29;
   int val = FEA(base, e, m);
-  cout << x << "^" << e << " % " << m << " = " << val << endl;
+  cout << base << "^" << e << " % " << m << " = " << val << endl;
 
   // Test Z_n code.
   int n = 14;
@@ -119,12 +187,19 @@ int main(int argc, char *argv[]) {
       cout << Z.at(i) << " ";
   cout << "}" << endl;
 
-
   // Test Primitive Roots algorithms.
   vector<int> primitive_roots = primitiveRoots(26);
   cout << "primitive_roots = { ";
   for ( int i = 0 ; i < primitive_roots.size() ; i++ )
       cout << primitive_roots.at(i) << " ";
   cout << "}" << endl;
+
+  // Test Baby-step Giant-step algorithm.
+  int b = 2;
+  x = 3;
+  int p = 101;
+  val = BSGS(b, x, p);
+  cout << val << endl;
+
 }
 
